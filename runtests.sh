7b2834ed3f68cfbb380884dbe098b8144ab3e4b7
Nelson Yeung <nelsyeung@icloud.com>
2017-01-13 15:08:42 +0000

    Add run single test to runtests script
    - allow the ability to run just a single test by supplying an argument.

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,10 @@
-export PYTHONPATH="./teptools:$PYTHONPATH" &&
+export PYTHONPATH="./teptools:$PYTHONPATH"
+
+if [[ -z $1 ]]; then
 	flake8 teptools tests &&
 	coverage run -m py.test tests &&
 	coverage report
+else
+	flake8 teptools/$1.py tests/test_$1.py &&
+	pytest tests/test_$1.py
+fi
